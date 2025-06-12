7ffd9391e4c867a7149c3b9801cf1cca256d7050
Belén de Amorrortu <belen@bewise.com.es>
2025-06-11 21:30:43 -0300

    refactor: remove icon logic from theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,9 @@
 function setTheme(isDark) {
   const html = document.documentElement;
-  const themeIcon = document.querySelector(".theme-toggle i");
-
   if (isDark) {
     html.classList.add("dark");
-    themeIcon.className = "fas fa-sun";
-    themeIcon.title = "Cambiar a modo claro";
   } else {
     html.classList.remove("dark");
-    themeIcon.className = "fas fa-moon";
-    themeIcon.title = "Cambiar a modo oscuro";
   }
 }
 
@@ -24,10 +18,11 @@ function loadTheme() {
   setTheme(savedTheme === "dark");
 }
 
+loadTheme();
+
 document.addEventListener("DOMContentLoaded", function () {
   const themeButton = document.querySelector(".theme-toggle");
   if (themeButton) {
     themeButton.addEventListener("click", toggleTheme);
-    loadTheme();
   }
 });
